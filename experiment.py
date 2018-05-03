d9b5d560a21a7c479fce1e5fa41aca8be9a97033
Qingyu Deng <bitdqy@hotmail.com>
2018-05-03 10:27:18 +0100

    Add Experiment class

---
+++ experiment.py
@@ -0,0 +1,68 @@
+""" Experiment Module
+
+A class of experiment is defined here.
+"""
+class Experiment:
+    """Defines an experiment.
+    
+    Raises:
+        RuntimeError -- If the lengths of parameters are consistant.
+    
+    """
+
+    def __init__(self, parameters, experiment_code, result_processor):
+        """Initialize a experiment
+        
+        Arguments:
+            parameters {dict} -- A dict contains parameters.
+            experiment_code {callable} -- A callable object. It should 
+                                          accept arguments with same name of 
+                                          parameters, and return a dict
+                                          contains result.
+            result_processor {callable} -- A callable object. It should 
+                                           accept arguments with same name
+                                           of result. The return value will
+                                           be collect by this object and
+                                           can be accessed by look_results().
+        
+        Raises:
+            RuntimeError -- If the lengths of parameters are not consistant,
+                            RuntimeError would be raised.
+        """
+
+        len_parameters = [len(parameters[key]) for key in parameters]
+        if len(set(len_parameters)) != 1:
+            raise RuntimeError('The lengths of parameters are not consistant. \
+            The lengths of parameters you passed are {}.', len_parameters)
+        self._parameters = parameters
+        self._run_experiment = experiment_code
+        self._process_result = result_processor
+        self._results = []
+
+    def execute(self):
+        """Execute experiment
+        """
+
+        parameter_dicts = []
+        for key in self._parameters:
+            key_val_pairs = [{key: val} for val in self._parameters[key]]
+            if not parameter_dicts:
+                parameter_dicts = key_val_pairs
+            else:
+                for i, key_val_pair in enumerate(key_val_pairs):
+                    parameter_dicts[i].update(key_val_pair)
+        
+        for parameter_dict in parameter_dicts:
+            res = self._run_experiment(**parameter_dict)
+            processed_res = self._process_result(**res)
+            self._results.append(processed_res)
+
+    def look_results(self):
+        """Look result
+        
+        Returns:
+            [list] -- [results of experiment in order of defined parameters.]
+        """
+
+        return self._results
+        
---
+++ test/test_experiment.py
@@ -0,0 +1,41 @@
+from unittest import TestCase
+
+from experiment import Experiment
+
+class TestExperiment(TestCase):
+    def test_multiple_parameter_experiment(self):
+        # Parameter is a dict with the key is the name of input parameter
+        # and the element is a list of parameter.
+        # The number of element list is required to be same.
+        parameter_set = {'p1': [1, 2, 3, 4, 5], 'p2':[4, 5, 6, 7, 8]}
+
+        # Parameter defined in parameter_set should be catch by the named
+        # arguments in do_experiment
+        def do_experiment(p1, p2):
+            # Return value is a dict with the key is the category name of
+            # result
+            return {'sum_': p1 + p2 }
+
+        # Result returned by do_experiment should be catch by the named
+        # arguments in manipulate_result.
+        def manipulate_result(sum_):
+            return sum_ + 1
+
+        # The value returned by manipulated_result should be saved and be
+        # able to be accessed by look_results() method.
+
+        expr = Experiment(parameter_set, do_experiment, manipulate_result)
+        expr.execute()
+
+        result = expr.look_results()
+        self.assertEqual(result, [6, 8, 10, 12, 14])
+
+    def test_parameters_of_different_length(self):
+        """If the length of different parameters is not consistant,
+        experiment should throw an exception when initializing.
+        """
+        parameter_set = {'p1': [1, 2, 3, 4, 5], 'p2':[4, 5, 6, 7, 8, 9]}
+
+        with self.assertRaises(RuntimeError):
+            expr = Experiment(parameter_set, None, None)
+
